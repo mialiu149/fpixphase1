b23541f699f190a765f33082b8738da757d64301
mialiu149 <mliu@fpixp1hc.cern.ch>
2017-01-15 13:09:26 +0100

    remove stupid git commit

--- mkem_PCX.sh
+++
@@ -1,5 +0,0 @@
-#
-${BUILD_HOME}/pixel/bin/PixelConfigDBCmd.exe --insertConfigAlias $1 detconfig $3 \
-portcardmap Default nametranslation Default fedcard Default dac Default lowvoltagemap Default maxvsf Default mask Default \
-trim Default tbm Default dcdc Default portcard Default fedconfig Default fecconfig Default tkfecconfig Default \
-ttcciconfig Default ltcconfig Default amc13 Default globaldelay25 Default calib $2
